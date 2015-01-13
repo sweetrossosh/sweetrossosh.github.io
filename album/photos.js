d4e9ed4d6ee2ed57f057d543bc7452f52db0c02d
hedhyw <hedhyw@aol.com>
2015-01-13 20:51:08 +0300

    gh-pages

---
+++ album/photos.js
@@ -0,0 +1,7 @@
+var names = ["../photos/1.jpg", "../photos/2.jpg",
+			 "../photos/3.jpg", "../photos/4.jpg",
+			 "../photos/5.jpg", "../photos/6.jpg",
+			 "../photos/7.jpg", "../photos/8.jpg",
+			 "../photos/9.jpg", "../photos/10.jpg",
+			 "../photos/11.jpg", "../photos/12.jpg",
+			 "../photos/13.jpg"];
