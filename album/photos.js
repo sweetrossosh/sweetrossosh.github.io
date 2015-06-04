b4447714aa298453d8ce4f9df362f2ea2e941cb7
hedhyw <hedhyw@aol.com>
2015-06-04 19:43:41 +0300

    gh-pages

--- album/photos.js
+++ album/photos.js
@@ -17,8 +17,8 @@ var names = [["../photos/difficult/58R35XON.jpg", 0],
 			 ["../photos/other/U2KWFIRC.jpg", 3],
 			 ["../photos/other/ZJGV530W.jpg", 3],
 			 ["../photos/photocakes/TSK2UVN8.jpg", 4],
-			 ["../photos/childrens/OXICJTNK.jpg", 4],
-			 ["../photos/childrens/HAKHLYMD.jpg", 4]];
+			 ["../photos/photocakes/OXICJTNK.jpg", 4],
+			 ["../photos/photocakes/HAKHLYMD.jpg", 4]];
 			 
 names.sort(function sf(A, B){
 	return A[1] > B[1];
